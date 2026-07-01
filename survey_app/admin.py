24beb68523c6cfc1701cde3d58c0c8559b40c60d
jovishacurlie <jovishacurlie@gmail.com>
2026-07-01 12:45:41 +0530

    changed admin.py

--- survey_app/admin.py
+++ survey_app/admin.py
@@ -15,9 +15,29 @@ admin.site.register(ParticipantSession)
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(MovieSelection)
-admin.site.register(WebcamClip)
-admin.site.register(ScreenClip)
 admin.site.register(MovieReviewResponse)
+
+
+@admin.register(WebcamClip)
+class WebcamClipAdmin(admin.ModelAdmin):
+    list_display = ("id", "participant", "clip", "created_at")
+    list_select_related = ("participant",)
+    list_filter = ("created_at",)
+    search_fields = ("participant__id", "participant__user__username", "clip")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+
+@admin.register(ScreenClip)
+class ScreenClipAdmin(admin.ModelAdmin):
+    list_display = ("id", "participant", "clip", "created_at")
+    list_select_related = ("participant",)
+    list_filter = ("created_at",)
+    search_fields = ("participant__id", "participant__user__username", "clip")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+
+
 @admin.register(PaasResponse)
 class PaasResponseAdmin(admin.ModelAdmin):
     list_display = ('participant', 'task_number', 'rating', 'created_at')
